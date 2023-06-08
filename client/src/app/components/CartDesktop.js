12d21667fc4aa47f64002ed4b1a7bc5e3cc74287
Dahman <94912743+DahmanCode@users.noreply.github.com>
2023-06-08 12:40:28 +0100

    add Cart Desktop

--- client/src/app/components/CartDesktop.js
+++ client/src/app/components/CartDesktop.js
@@ -1,5 +1,24 @@
+'use client'
+import React, {useContext} from "react";
+// components
+import CartItem from "./CartItem";
+import CartBottom from "./CartBottom";
+import CartTop from "./CartTop";
+// context
+import { CartContext } from '../context/CartContext.js'
+
+
 const CartDesktop = () => {
-  return <div>CartDesktop</div>;
+  const { isOpen, cart } = useContext(CartContext)
+  return (
+    <div 
+      className={`${
+        isOpen ? 'left-0' : '-left-full'
+      } bg-white fixed top-0 bottom-0 w-[500px] shadow-2xl hidden lg:flex flex-col transition-all duration-300`}
+    >
+      
+    </div>
+  );
 };
 
 export default CartDesktop;
--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -5,6 +5,7 @@ import './globals.css';
 import Nav from './components/Nav';
 import CartMobileIcon from './components/CartMobileIcon';
 import CartMobile from './components/CartMobile';
+import CartDesktop from './components/CartDesktop';
 
 // provider
 import CartProvider from './context/CartContext';
@@ -39,6 +40,7 @@ export default function RootLayout({ children }) {
           <CartMobileIcon />
           <CartMobile />
           {children}
+          <CartDesktop />
 
         </body>
       </html>
